65f994395d39ec1c037a87b8c9a374a5c8694823
Nicolas Camhi <nicolascamhi@Nicolas-MacBook-Air.local>
2023-11-20 00:29:00 -0300

    feat: email validation

--- src/componentes/crearReunion.js
+++ src/componentes/crearReunion.js
@@ -6,10 +6,23 @@ import Select from 'react-select';
 import '../styles/crearReunion.css';
 
 function CrearReunion() {
-  const [fechaCreacion, setFechaCreacion] = useState('');
-  const [fechaReunion, setFechaReunion] = useState('');
+  const [fechaCreacion, setFechaCreacion] = useState(new Date());
+  const [fechaReunion, setFechaReunion] = useState(new Date());
   const [cliente, setCliente] = useState('');
-  const [tamanoEmpresa, setTamanoEmpresa] = useState('');
+  const [tamanoEmpresa, setTamanoEmpresa] = useState(null);
+  const [correoContacto, setCorreoContacto] = useState('');
+  const [correoValido, setCorreoValido] = useState(true);
+
+  const validarCorreo = (correo) => {
+    const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regexCorreo.test(correo);
+  };
+
+  const handleCorreoChange = (e) => {
+    const correo = e.target.value;
+    setCorreoContacto(correo);
+    setCorreoValido(validarCorreo(correo));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,6 +33,7 @@ function CrearReunion() {
         fechaReunion,
         cliente,
         tamanoEmpresa,
+        correoContacto, // Incluir el correo de contacto en los datos a enviar
         // Agrega más atributos aquí
       });
       // Manejar la respuesta o redirigir
@@ -33,46 +47,28 @@ function CrearReunion() {
     <div className="form-container">
       <h2 className="text-2xl font-semibold mb-4">Crear Reunión</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-    <label htmlFor="fechaCreacion" className="form-label">
-        Fecha de Creación:
-    </label>
-    <DatePicker
-        selected={fechaCreacion}
-        onChange={(date) => setFechaCreacion(date)}
-        id="fechaCreacion"
-        className="form-input"
-    />
-    </div>
         <div>
-        <label htmlFor="fechaReunion" className="form-label">
+          <label htmlFor="fechaCreacion" className="form-label">
+            Fecha de Creación:
+          </label>
+          <DatePicker
+            selected={fechaCreacion}
+            onChange={(date) => setFechaCreacion(date)}
+            id="fechaCreacion"
+            className="form-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="fechaReunion" className="form-label">
             Fecha de Reunión:
-        </label>
-        <DatePicker
+          </label>
+          <DatePicker
             selected={fechaReunion}
             onChange={(date) => setFechaReunion(date)}
             id="fechaReunion"
             className="form-input"
-        />
+          />
         </div>
-
-        <div>
-        <label htmlFor="tamanoEmpresa" className="form-label">
-            Tamaño de la Empresa:
-        </label>
-        <Select
-            id="tamanoEmpresa"
-            value={tamanoEmpresa}
-            onChange={(selectedOption) => setTamanoEmpresa(selectedOption)}
-            options={[
-            { value: 'Pequeña', label: 'Pequeña' },
-            { value: 'Mediana', label: 'Mediana' },
-            { value: 'Grande', label: 'Grande' },
-            ]}
-            className="form-input"
-        />
-        </div>
-
         <div>
           <label htmlFor="cliente" className="form-label">
             Cliente:
@@ -83,6 +79,33 @@ function CrearReunion() {
             value={cliente}
             onChange={(e) => setCliente(e.target.value)}
             className="form-input"
+          />
+        </div>
+        <div>
+          <label htmlFor="tamanoEmpresa" className="form-label">
+            Tamaño de la Empresa:
+          </label>
+          <Select
+            id="tamanoEmpresa"
+            value={tamanoEmpresa}
+            onChange={(selectedOption) => setTamanoEmpresa(selectedOption)}
+            options={[
+              { value: 'Pequeña', label: 'Pequeña' },
+              { value: 'Mediana', label: 'Mediana' },
+              { value: 'Grande', label: 'Grande' },
+            ]}
+          />
+        </div>
+        <div>
+          <label htmlFor="correoContacto" className="form-label">
+            Correo de Contacto:
+          </label>
+          <input
+            type="email"
+            id="correoContacto"
+            value={correoContacto}
+            onChange={handleCorreoChange}
+            className={`form-input ${correoValido ? '' : 'invalid'}`}
           />
         </div>
         <button type="submit" className="form-button">
